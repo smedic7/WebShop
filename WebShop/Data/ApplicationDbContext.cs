851637f7ed2e7fd8271d873385b68ab98cc65b83
hradmin_sdedic <hradmin_sdedic@HRZAGWN11723142>
2023-06-20 19:46:16 +0200

    Add project files.

---
+++ WebShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+
+
+        }
+    }
+
+        public class ApplicationUser :IdentityUser
+        {
+            [StringLength(50)]
+            public string FirstName { get;set; }
+            [StringLength(50)]
+            public string LastName { get;set; }
+            [StringLength(150)]
+            public string Address { get;set; }
+
+
+
+        }
+
+
+
+
+
+
+}
