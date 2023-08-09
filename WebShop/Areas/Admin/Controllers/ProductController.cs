75f0cab6069b65b7125f6f852538e2931f2f3cbf
sdedic <sasa.dedic@gmail.com>
2023-08-09 15:58:01 +0200

    addingCRUDonProduct

--- WebShop/Areas/Admin/Controllers/ProductController.cs
+++ WebShop/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Data;
 using WebShop.Data;
 using WebShop.Models;
@@ -32,6 +33,39 @@ namespace WebShop.Areas.Admin.Controllers
         }
 
 
+
+        public IActionResult Details(int id)
+        {
+            if(id == 0)
+            {
+
+                return NotFound();
+            }
+
+
+            var product = _dbContext.Products.FirstOrDefault(x => x.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+           
+
+            return View(product);
+            
+            
+            
+            
+
+
+        }
+
+
+
+
+
+
+
         [HttpGet]
         public IActionResult Create()
         {
@@ -45,27 +79,115 @@ namespace WebShop.Areas.Admin.Controllers
         public IActionResult Create(Product product)
 
         {
-            
-            if(ModelState.IsValid)
+
+            if (ModelState.IsValid)
             {
-                _dbContext.Products.Add(product);   
+                _dbContext.Products.Add(product);
                 _dbContext.SaveChanges();
 
-                return RedirectToAction("Index");    
-                    
-                    
+                return RedirectToAction("Index");
+
+
             }
-            
-            
+
+
             return View(product);
+        }
+         
+
+            
+        public IActionResult Edit(int id) 
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            var product= _dbContext.Products.FirstOrDefault(p => p.Id == id);
+            
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+
+
+
+        public IActionResult Edit(Product product) 
+        {
+            if(product == null || product.Id ==0)
+            {
+                return NotFound();
+            }
+
+            if(ModelState.IsValid)
+            {
+                _dbContext.Products.Update(product);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+         
+            return View(product);
+            
+        }
+        
+        
+
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+        
+            var product = _dbContext.Products.FirstOrDefault(p => p.Id==id);
+
+            if (product == null)
+            {
+
+                return NotFound();
+            }
+        
+        
+        
+        
+            return View(product);
+        }
+
+        [HttpPost]
+
+
+        public IActionResult DeleteConfirm(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
             }
 
 
+            var product = _dbContext.Products.FirstOrDefault(p =>p.Id==id);
+
+            
+            if(product == null)
+            {
+
+                return NotFound();  
+            }
+            
+            
+            _dbContext.Remove(product);
+            _dbContext.SaveChanges();
 
 
-
-
-
+            return RedirectToAction("Index");
+        }
+      
+        
 
 
     }
