6bee1e6bc027fc74a32af77910a70470dd3b49d2
sdedic <sasa.dedic@gmail.com>
2023-09-07 19:24:53 +0200

    addedWebApiController

---
+++ WebShop/WebAPI/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebShop.Data;
+using WebShop.Models;
+
+namespace WebShop.WebAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductController(ApplicationDbContext dbContext)
+        {
+
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _dbContext.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+
+
+
+    }
+}
